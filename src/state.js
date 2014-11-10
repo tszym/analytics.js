cbd711152940145ab0a0a37ceaa2c5566a3b5727
Thomas Robert <trobert94@gmail.com>
2014-11-10 14:35:45 +0100

    Complete state namespace

--- src/state.js
+++ src/state.js
@@ -1,206 +1,285 @@
-analytics.state = {};
+analytics.state = (function() {
 
-var state = analytics.state;
+  var state = function (state) {
+    if (!arguments.length) return getState();
+    setState(state);
+  };
 
-var _schema = null;
-var _cube = null;
-var _measure = null;
-var _dimensions = [];
+  var _schema     = null;
+  var _cube       = null;
+  var _measure    = null;
+  var _cubeObj    = null;
+  var _measureObj = null;
+  var _dimensions = [];
 
+  /*
+   * Getters and setters
+   */
+  state.schema = function(schema) {
+    if (!arguments.length) return _schema;
+    _schema = schema;
+  };
 
-state.schema = function(schema) {
-  if (!arguments.length) return _schema;
-  _schema = schema;
-};
+  state.cube = function(cube) {
+    if (!arguments.length) return _cubeObj;
+    _cubeObj = cube;
+    _cube = cube.id();
+  };
 
-state.cube = function(cube) {
-  if (!arguments.length) return _cube;
-  _cube = cube;
-};
+  state.measure = function(measure) {
+    if (!arguments.length) return _measureObj;
+    _measureObj = measure;
+    _measure = measure.id();
+  };
 
-state.measure = function(measure) {
-  if (!arguments.length) return _measure;
-  _measure = measure;
-};
+  state.dimensions = function() {
+    return _dimensions;
+  };
 
-state.dimensions = function() {
-  return _dimensions;
-};
+  /**
+   * Callback function to call when changing the cube and measure to display
+   *
+   * @private
+   * @param {string} cube
+   * @param {string} measure
+   */
+  function setCubeAndMeasureCallback(cube, measure) {
 
-/**
- * Callback function to call when changing the cube and measure to display
- *
- * @private
- * @param {string} cube
- * @param {string} measure
- * @todo
- */
-function setCubeAndMeasureCallback(cube, measure) {
+    // changing cube = reset all
+    if (!state.cube().equals(cube)) {
+      state.cube(cube);
+      state.measure(measure);
 
-  // changing cube = reset all
-  if (cube != _cube) {
-    _cube = cube;
-    _measure = measure;
-    _dimensions = [];
-    analytics.display.reset();
-    initMetadata();
-    analytics.data.load();
-    analytics.display.render();
-  }
-  else {
-    _measure = measure;
-    analytics.data.load();
-    analytics.display.redraw();
-  }
-}
-
-
-/**
- * Initialise schema, cube and measure and display the fact selector
- * Selects first schema, cube and measure if not set by setState
- *
- * @throws {Exception} if problem during Query
- * @private
- */
-function initMeasure() {
-  // select first schema if unset of unexistant
-  var schemas = Query.getSchemas();
-  if (_schema === null || schemas[_schema] === undefined)
-    _schema = Object.keys(schemas)[0];
-
-  // get measures by cubes
-  var cubesAndMeasures = Query.getCubesAndMeasures(_schema);
-
-  // select first cube if unset of unexistant
-  if (_cube === null || cubesAndMeasures[_cube] === undefined)
-    _cube = Object.keys(cubesAndMeasures)[0];
-
-  // select first measure if unset of unexistant
-  if (_measure === null || cubesAndMeasures[_cube].measures[_measure] === undefined)
-    _measure = Object.keys(cubesAndMeasures[_cube].measures)[0];
-}
-
-/**
- * Initialize the metadatas according to the selected cube, ie get dimensions (standard and particular ones),
- * hierarchies, assign them to the charts.
- *
- * Slice the dimensions on for the members of the first level
- *
- * @throws {Exception} if problem during Query
- * @private
- */
-function initMetadata() {
-
-  if (analytics.display.initNecessary()) {
-
-    // get specific infos
-    var geoDimension  = Query.getGeoDimension(_schema, _cube);
-    // var timeDimension = Query.getTimeDimension(_schema, _cube);
-    // var geoHierarchy = Object.keys(Query.getHierarchies(_schema, _cube, geoDimension))[0];
-
-    // slice all dimensions by default
-    var dimensions = Query.getDimensions(_schema, _cube);
-    for (var dimension in dimensions) {
-      var hierarchy  = Object.keys(Query.getHierarchies(_schema, _cube, dimension))[0];
-      var properties = dimension == geoDimension;
-      var members    = Query.getMembers(_schema, _cube, dimension, hierarchy, 0, properties);
-
-      var dimensionObj = analytics.dimension(dimension, dimensions[dimension].caption, hierarchy);
-      dimensionObj.addSlice(members);
-      _dimensions.push(dimensionObj);
-    }
-
-    // TODO add good parameters
-    analytics.display.initCharts();
-  }
-}
-
-/**
- * Drill down on the given dimension on a member. Should called inside callback functions.
- * Will update the charts consequently.
- *
- * @private
- * @param {analytics.dimension} dimension id of the dimension on which we want to drill down
- * @param {string} member id of the member on which we want to drill down
- * @param {string} dcChartID id of the dc chart on which the evenement was called
- */
-function drillDown (dimension, member, dcChartID) {
-  try {
-    var hierarchy = this.getDimensionHierarchy(dimension);
-    var oldLevel  = this.getDimensionCurrentLevel(dimension);
-
-    if (dimension.isDrillPossible()) {
-
-      // zoom on charts using the dimensions
-      analytics.display.getChartsUsingDimension(dimension).forEach(function (chart) {
-        if (chart.element()._onZoomIn !== undefined && chart.element().chartID() !== dcChartID) {
-          chart.element()._onZoomIn(member);
-        }
-      });
-
-      // add slice to stack
-      var newMembers = Query.getMembers(_schema, _cube, dimension.id(), dimension.hierarchy(), dimension.currentLevel(), true, member);
-      dimension.addSlice(newMembers);
-
-      // reset filter on charts using this dimension
-      analytics.display.filterAllChartsUsingDimension(dimension);
+      _dimensions = [];
+      state.initDimensions();
       analytics.data.load();
       analytics.display.render();
     }
+    else {
+      state.measure(measure);
+
+      analytics.data.load();
+      analytics.display.redraw();
+    }
   }
-  catch(err) {
-    new PNotify({
-      title: 'An error occured',
-      type: 'error',
-      text: err.message
-    });
-  }
-}
 
-/**
- * Roll up on the given dimension. Should called inside callback functions.
- * Will update the charts consequently.
- *
- * @private
- * @param {string} dimension id of the dimension on which we want to roll up
- * @param {string} dcChartID id of the dc chart on which the evenement was called
- * @param {integer} [nbLevels=1] number of levels to roll up
- */
-function rollUp (dimension, dcChartID, nbLevels) {
-  nbLevels = nbLevels || 1;
+  /**
+   * Initialize schema, cube and measure and display the fact selector
+   * Selects first schema, cube and measure if not set by setState
+   *
+   * @throws {Exception} if problem during analytics.query
+   * @private
+   */
+  state.initMeasure = function () {
 
-  try {
-    // do not allow full projection of a dimension
-    if (dimension.isRollPossible()) {
+    // select first schema if unset of unexistant
+    var schemas = analytics.query.getSchemas();
+    if (_schema === null || schemas[_schema] === undefined)
+      _schema = Object.keys(schemas)[0];
 
-      var zoomOut = function (chart) {
-        if (chart.element()._onZoomOut !== undefined && chart.element().chartID() !== dcChartID) {
-          chart.element()._onZoomOut();
+    // get measures by cubes
+    var cubesAndMeasures = analytics.query.getCubesAndMeasures(_schema);
+
+    // select first cube if unset of unexistant
+    if (_cube === null || cubesAndMeasures[_cube] === undefined) {
+      var cubeId = Object.keys(cubesAndMeasures)[0];
+      state.cube(analytics.data.cube(cubeId, cubesAndMeasures[cubeId].caption));
+    }
+
+    // select first measure if unset of unexistant
+    if (_measure === null || cubesAndMeasures[_cube].measures[_measure] === undefined) {
+      var measureId = Object.keys(cubesAndMeasures[_cube].measures)[0];
+      state.measure(analytics.data.measure(measureId, cubesAndMeasures[_cube].measures[measureId].caption));
+    }
+
+    analytics.display.showFactSelector(cubesAndMeasures, state.cube(), state.measure(), setCubeAndMeasureCallback);
+  };
+
+  /**
+   * Initialize the metadatas according to the selected cube, ie get dimensions (standard and particular ones),
+   * hierarchies, assign them to the charts.
+   *
+   * Slice the dimensions on for the members of the first level
+   *
+   * @throws {Exception} if problem during analytics.query
+   * @private
+   */
+  state.initDimensions = function () {
+    // TODO shouldn't creating dimension objects be done by analytics.query?
+
+    if (!_dimensions.length) {
+      // get specific infos
+      var geoDimension  = analytics.query.getGeoDimension(_schema, _cube);
+      var timeDimension = analytics.query.getTimeDimension(_schema, _cube);
+      var geoDimensionObj, timeDimensionObj;
+
+      // slice all dimensions by default
+      var dimensions = analytics.query.getDimensions(_schema, _cube);
+      for (var dimension in dimensions) {
+        var hierarchy  = Object.keys(analytics.query.getHierarchies(_schema, _cube, dimension))[0];
+        var properties = [];
+        if (dimension == geoDimension) {
+          var propertiesMap = analytics.query.getProperties(_schema, _cube, dimension, hierarchy, 0);
+          var propertyId = analytics.query.getGeoProperty(_schema, _cube, dimension, hierarchy);
+          properties.push(analytics.data.property(propertyId, propertiesMap[propertyId].caption, propertiesMap[propertyId].type));
         }
-      };
+        var levels     = analytics.query.getLevels(_schema, _cube, dimension, hierarchy);
+        var members    = analytics.query.getMembers(_schema, _cube, dimension, hierarchy, 0, properties.length > 0);
 
-      // roll up nbLevels times
-      for (var i = 1; i <= nbLevels; i++) {
-        // zoom out on charts
-        analytics.display.getChartsUsingDimension(dimension).forEach(zoomOut);
+        var dimensionObj = analytics.data.dimension(dimension, dimensions[dimension].caption, dimensions[dimension].type, hierarchy, levels, properties);
+        dimensionObj.addSlice(members);
+        _dimensions.push(dimensionObj);
 
-        // remove last slice
-        dimension.removeLastSlice();
+        // save import dims
+        if (dimensionObj.type() == "Geometry")
+          geoDimensionObj = dimensionObj;
+        else if (dimensionObj.type() == "Time")
+          timeDimensionObj = dimensionObj;
       }
 
-      // reset filter on charts using this dimension
-      analytics.display.filterAllChartsUsingDimension(dimension);
+      // asign those dimensions to charts
+      analytics.display.assignDimensions(_dimensions, geoDimensionObj, timeDimensionObj);
+    }
 
-      // regenerate all
+    // create wordclouds
+    analytics.display.createWordClouds(_dimensions);
+  };
+
+  /**
+   * Drill down on the given dimension on a member. Should called inside callback functions.
+   * Will update the charts consequently.
+   *
+   * @private
+   * @param {analytics.dimension} dimension id of the dimension on which we want to drill down
+   * @param {string} member id of the member on which we want to drill down
+   * @param {string} dcChartID id of the dc chart on which the evenement was called
+   */
+  state.drillDown = function (dimension, member) {
+
+    if (dimension.isDrillPossible()) {
+      var newMembers = analytics.query.getMembers(_schema, _cube, dimension.id(), dimension.hierarchy(), dimension.currentLevel(), dimension.properties().length > 0, member);
+      dimension.addSlice(newMembers);
       analytics.data.load();
-      analytics.display.render();
+    }
+  };
+
+  /**
+   * Roll up on the given dimension. Should called inside callback functions.
+   * Will update the charts consequently.
+   *
+   * @private
+   * @param {string} dimension id of the dimension on which we want to roll up
+   * @param {integer} [nbLevels=1] number of levels to roll up
+   */
+  state.rollUp = function (dimension, nbLevels) {
+    nbLevels = nbLevels || 1;
+    nbLevels = Math.min(nbLevels, dimension.nbRollPossible());
+
+    if (nbLevels > 0) {
+
+      // remove last slice nbLevels times
+      for (var i = 0; i < nbLevels; i++)
+        dimension.removeLastSlice();
+
+      // reload data
+      analytics.data.load();
+    }
+  };
+
+  function getState() {
+    // init output
+    var out = {
+      "schema"       : analytics.state.schema(),
+      "cube"         : analytics.state.cube().id(),
+      "measure"      : analytics.state.measure().id(),
+      "columnWidths" : analytics.display.columnWidths()
+    };
+
+    // list dimensions
+    out.dimensions = analytics.state.dimensions().map(function (dimension) {
+      return {
+        id           : dimension.id(),
+        hierarchy    : dimension.hierarchy(),
+        filters      : dimension.filters(),
+        properties   : dimension.properties().map(function (property) { return property.id(); }),
+        membersStack : dimension.membersStack().map(function (members) { return Object.keys(members); })
+      };
+    });
+
+    // list charts
+    out.charts = analytics.display.chartsInLayout().map(function (chartsCol, i) {
+      if (i > 0) { // do not save wordclouds
+        return chartsCol.map(function (chart) {
+          return {
+            type          : chart.type(),
+            options       : chart.options(),
+            dimensions    : chart.dimensions()   .map(function (dimension) { return dimension.id(); }),
+            extraMeasures : chart.extraMeasures().map(function (measure)   { return measure  .id(); })
+          };
+        });
+      }
+      else {
+        return [];
+      }
+    });
+
+    return out;
+  }
+
+  function setState(savedState) {
+
+    try {
+
+      // schema
+      state.schema(savedState.schema);
+
+      // cube
+      var cubes = analytics.query.getCubes(savedState.schema);
+      state.cube(analytics.data.cube(savedState.cube, cubes[savedState.cube]));
+
+      // measure
+      var measuresMap = {};
+      var measures = analytics.query.getMesures(savedState.schema, savedState.cube);
+      for (var measure in measures) {
+        measuresMap[measure] = analytics.data.measure(measure, measures[measure].caption);
+      }
+      state.measure(measuresMap[savedState.measure]);
+
+      // columns
+      analytics.display.columnWidths(savedState.columnWidths);
+
+      // dimensions
+      var dimensionsMap = {};
+      var dimensions = analytics.query.getDimensions(savedState.schema, savedState.cube);
+      savedState.dimensions.forEach(function (dimension) {
+        var levels = analytics.query.getLevels(savedState.schema, savedState.cube, dimension.id, dimension.hierarchy);
+        var propertiesMap = analytics.query.getProperties(savedState.schema, savedState.cube, dimension.id, dimension.hierarchy, 0);
+
+        var properties = dimension.properties.map(function (property) {
+          return analytics.data.property(property, propertiesMap[property].caption, propertiesMap[property].type);
+        });
+
+        var dimensionObj = analytics.data.dimension(dimension.id, dimensions[dimension.id].caption, dimensions[dimension.id].type, dimension.hierarchy, levels, properties);
+        dimensionObj.filters(dimension.filters);
+        dimension.membersStack.forEach(function (members, levelId) {
+          dimensionObj.addSlice(analytics.query.getMembersInfos(savedState.schema, savedState.cube, dimension.id, dimension.hierarchy, levelId, members, dimension.properties));
+        });
+
+        _dimensions.push(dimensionObj);
+        dimensionsMap[dimensionObj.id()] = dimensionObj;
+      });
+
+      // charts
+      analytics.display.createCharts(savedState.charts, dimensionsMap, measuresMap);
+    }
+    catch(err) {
+      new PNotify({
+        title: 'Data for this analysis is unavailable',
+        type: 'error'
+      });
     }
   }
-  catch(err) {
-    new PNotify({
-      title: 'An error occured',
-      type: 'error',
-      text: err.message
-    });
-  }
-}
+  
+  return state;
+
+})();
