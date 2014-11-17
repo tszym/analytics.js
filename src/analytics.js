9c3063228c5a5ae58e4febddf3eb91c51397b700
Thomas Robert <trobert94@gmail.com>
2014-11-17 20:35:51 +0100

    Add constants to analytics

--- src/analytics.js
+++ src/analytics.js
@@ -1,5 +1,31 @@
 var analytics = {
   version: '%VERSION%',
-  csts : {}
+  csts : {
+    resizeDelay : 350,
+    css : {
+      columnsContainer : '#columns',
+      columns          : '.chart-columns',
+      charts           : '.chart',
+      chartsClass      : 'chart',
+      factSelector     : '#fact-selector',
+      reset            : '#reset',
+      zoom             : 'zoom'
+    },
+    txts : {
+      charts : {
+        map : 'Choropleth map',
+        bar : 'Bar chart',
+        pie : 'Pie chart',
+        timeline : 'Timeline',
+        bubble : 'Bubble chart',
+        table : 'Table',
+        wordcloud : 'Word cloud chart'
+      },
+      factSelector : {
+        cubes    : 'Cubes available:',
+        measures : 'Measures available:'
+      }
+    }
+  }
 };
 
