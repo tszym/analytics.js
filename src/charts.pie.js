440bf1ef0511f8728045241238cc1108ab9f750e
Thomas Robert <trobert94@gmail.com>
2014-11-23 21:58:20 +0100

    Add charts.pie

---
+++ src/charts.pie.js
@@ -0,0 +1,33 @@
+analytics.charts.pie = (function () {
+  var pieChart = function (selector, dimensions) {
+
+    var _chart = analytics.charts.chart(selector, dimensions);
+
+    _chart.type = function() {
+      return "pie";
+    };
+
+    _chart._createDcElement = function () {
+      _chart._element = dc.pieChart(_chart.selector()+" .chart-container");
+    };
+
+    _chart._initChartSpecific = function () {
+      _chart.element()
+        .minAngleForLabel(0.3)
+        .ordering(function (d) { return d.value; });
+    };
+
+    _chart._resizeSpecific = function () {
+      _chart.element()
+        .radius(0); // force computation of pie size, useful when resizing
+    };
+
+    return _chart;
+  };
+
+  pieChart.options = {
+    sort : "valueasc"
+  };
+
+  return analytics.charts.chart.extend(pieChart);
+})();
